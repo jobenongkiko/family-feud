c378ecc12ac68997081f825af70ed90def5caec1
jongkiko <joben.ongkiko@corelogic.com.au>
2024-10-29 13:26:00 +0800

    add get all questions

--- backend/src/controllers/questionController.ts
+++ backend/src/controllers/questionController.ts
@@ -4,6 +4,15 @@ import { QuestionService } from '../services/questionService';
 export class QuestionController {
   private questionService: QuestionService = new QuestionService();
 
+  getQuestions: RequestHandler = async (_, res) => {
+    try {
+      const result = await this.questionService.getQuestions();
+      res.status(200).json(result);
+    } catch (error: any) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
   getQuestionWithAnswers: RequestHandler = async (req, res) => {
     try {
       const result = await this.questionService.getQuestionWithAnswers(req);
--- backend/src/routes/questionRoute.ts
+++ backend/src/routes/questionRoute.ts
@@ -4,6 +4,7 @@ import { QuestionController } from '../controllers/questionController';
 const router = Router();
 const questionController: QuestionController = new QuestionController(); 
 
+router.get('/', questionController.getQuestions);
 router.get('/:questionId', questionController.getQuestionWithAnswers);
 router.post('/add', questionController.addQuestion);
 
