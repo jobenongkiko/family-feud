8daa1fedf16cf8b230aa4b2243448a61db5a393f
jongkiko <joben.ongkiko@corelogic.com.au>
2024-10-29 21:19:23 +0800

    add dynamic api base url

--- frontend/src/api/questionApi.ts
+++ frontend/src/api/questionApi.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
+const URL = import.meta.env.VITE_AXIOS_BASE_URL;
 
 export const getQuestions = async () => {
-  const res = await axios.get<Question[]>('http://localhost:3000/question');
+  const res = await axios.get<Question[]>(`${URL}/question`);
   return res.data;
 }
 
 export const getQuestionWithAnswers = async (id: string) => {
-  const res = await axios.get<QuestionWithAnswers>(`http://localhost:3000/question/${id}`);
+  const res = await axios.get<QuestionWithAnswers>(`${URL}/question/${id}`);
   return res.data;
 }
