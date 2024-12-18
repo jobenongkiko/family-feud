729bb9da33e9a97d0f384b82dec2f7a38e4807e9
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2024-12-19 00:08:33 +0800

    add scrollable room list

--- frontend/src/pages/player/JoinRoom.tsx
+++ frontend/src/pages/player/JoinRoom.tsx
@@ -1,5 +1,7 @@
 import { useGetRoomsQuery } from '@/hooks/api-hooks/roomHook';
 import Loader from '@/components/root/Loader';
+import PrimaryButton from '@/components/root/PrimaryButton';
+
 import { useNavigate } from 'react-router-dom';
 
 const JoinRoom = () => {
@@ -11,20 +13,18 @@ const JoinRoom = () => {
     }
 
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 items-center w-full p-8 max-h-screen overflow-y-scroll">
             {rooms &&
                 rooms.map((room: Room) => (
-                    <span
+                    <PrimaryButton
                         key={room.uuid}
-                        className="bg-white text-red-600 w-fit"
+                        value={room.uuid.substring(0, 6)}
                         onClick={() =>
                             navigate(
                                 `/game/question/${room.question}?roomName=${room.roomName}&socketId=${room.socketId}`,
                             )
                         }
-                    >
-                        {room.uuid}
-                    </span>
+                    />
                 ))}
         </div>
     );
