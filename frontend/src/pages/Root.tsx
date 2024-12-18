3a1ca7b55e4b5b202d64c6ac2268b6e127186b4e
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2024-12-19 00:08:10 +0800

    add home button

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,10 +1,15 @@
+import { useLocation } from 'react-router';
 import { Outlet } from 'react-router';
 import AnimatedBackground from '@/components/root/AnimatedBackground';
+import HomeButton from '@/components/root/HomeButton';
 
 const Root = () => {
+    const location = useLocation();
+
     return (
         <main>
             <AnimatedBackground />
+            {location.pathname !== '/' && <HomeButton />}
             <Outlet />
         </main>
     );
